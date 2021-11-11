41f905dff70156f93a9d8429af7484a72f028e4f
pikiiaudina <pikiayu008@gmail.com>
2021-11-11 14:35:14 -0800

    tampilan halaman awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,41 +87,17 @@
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
                     <div class="section-heading">
-                        <h2>Choose <em>Program</em></h2>
-                        <img src="assets/images/line-dec.png" alt="waves">
-                        <p>Training Studio is free CSS template for gyms and fitness centers. You are allowed to use this layout for your business website.</p>
+                        <h2>Kantor Pertanahan Kabupaten <em>Banyuasin</em></h2><br>
+                        <h5>Alamat</h5>
+                        <p>Komplek, Jl. Perkantoran Pemkab, Pangkalan Balai, Kec. Banyuasin III, Kabupaten Banyu Asin, Sumatera Selatan 30911.</p>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <ul class="features-items">
                         <li class="feature-item">
-                            <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="First One">
-                            </div>
-                            <div class="right-content">
-                                <h4>Basic Fitness</h4>
-                                <p>Please do not re-distribute this template ZIP file on any template collection website. This is not allowed.</p>
-                                <a href="#" class="text-button">Discover More</a>
-                            </div>
-                        </li>
-                        <li class="feature-item">
-                            <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="second one">
-                            </div>
-                            <div class="right-content">
-                                <h4>New Gym Training</h4>
-                                <p>If you wish to support TemplateMo website via PayPal, please feel free to contact us. We appreciate it a lot.</p>
-                                <a href="#" class="text-button">Discover More</a>
-                            </div>
-                        </li>
-                        <li class="feature-item">
-                            <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="third gym training">
-                            </div>
-                            <div class="right-content">
-                                <h4>Basic Muscle Course</h4>
-                                <p>Credit goes to <a rel="nofollow" href="https://www.pexels.com" target="_blank">Pexels website</a> for images and video background used in this HTML template.</p>
-                                <a href="#" class="text-button">Discover More</a>
+                            <div class="right-content text-center">
+                                <h4>Definisi</h4>
+                                <p>Dengan adanya Sistem Informasi Inventaris Warkah ini dapat memudahkan kita untuk mencari data Sertifikat Tanah yang berada di Warkah.</p>  
                             </div>
                         </li>
                     </ul>
@@ -129,37 +105,19 @@
                 <div class="col-lg-6">
                     <ul class="features-items">
                         <li class="feature-item">
-                            <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="fourth muscle">
-                            </div>
-                            <div class="right-content">
-                                <h4>Advanced Muscle Course</h4>
-                                <p>You may want to browse through <a rel="nofollow" href="https://templatemo.com/tag/digital-marketing" target="_parent">Digital Marketing</a> or <a href="https://templatemo.com/tag/corporate">Corporate</a> HTML CSS templates on our website.</p>
-                                <a href="#" class="text-button">Discover More</a>
-                            </div>
-                        </li>
-                        <li class="feature-item">
-                            <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="training fifth">
-                            </div>
-                            <div class="right-content">
-                                <h4>Yoga Training</h4>
-                                <p>This template is built on Bootstrap v4.3.1 framework. It is easy to adapt the columns and sections.</p>
-                                <a href="#" class="text-button">Discover More</a>
-                            </div>
-                        </li>
-                        <li class="feature-item">
-                            <div class="left-icon">
-                                <img src="assets/images/features-first-icon.png" alt="gym training">
-                            </div>
-                            <div class="right-content">
-                                <h4>Body Building Course</h4>
-                                <p>Suspendisse fringilla et nisi et mattis. Curabitur sed finibus nisi. Integer nibh sapien, vehicula et auctor.</p>
-                                <a href="#" class="text-button">Discover More</a>
+                            <div class="right-content text-center">
+                                <h4>Institusi</h4>
+                                <p>Sistem Infromasi ini digunakan oleh pekerja yang ada di Kantor Pertanahan Kabupaten Banyuasin.</p>    
                             </div>
                         </li>
                     </ul>
                 </div>
+                            <div class="col-lg-12 text-center">
+                                <h5>Manfaat</h5><br>
+                                <p>1. Memudahkan pekerja dan klien saat mencari data Sertifikat Tanah.</p>
+                                <p>2. Mendapatkan Informasi secara Real-Time.</p>
+                                <p>3. Meminimalir Terjadinya Kecurangan.</p>
+                            </div>
             </div>
         </div>
     </section>
